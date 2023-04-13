3a502474ee4de183f8b460004f2372761195451e
huhuhu0420 <ghostcheng.00@gmail.com>
2023-04-13 09:35:26 +0800

    dart monkey upgrade

--- Source/Game/BtdClass/DartMonkey.cpp
+++ Source/Game/BtdClass/DartMonkey.cpp
@@ -6,6 +6,7 @@ namespace Btd
     DartMonkey::DartMonkey()
     {
         _range = 160;
+        _isPenetrate = false;
         ThrowablePath = {
             "resources/towers/monkey/dart_1.bmp", "resources/towers/monkey/dart_2.bmp",
             "resources/towers/monkey/dart_3.bmp", "resources/towers/monkey/dart_4.bmp",
@@ -28,13 +29,28 @@ namespace Btd
     void DartMonkey::PushThrowablePool()
     {
         auto dart = make_shared<Throwable>(Throwable());
-        if (_isUpgrade[0])
-        {
-            // dart monkey first upgrade can let throwable penetrate
-            dart->SetPenetrate(true);
-        }
+        dart->SetPenetrate(_isPenetrate);
         dart->LoadBitmapByString(ThrowablePath, RGB(255, 255, 255));
         dart->SetSpeed(5);
         throwablePool.push(dart);
+    }
+
+    void DartMonkey::Upgrade(int level)
+    {
+        if (!IsUpgrade[level])
+        {
+            switch (level)
+            {
+            case 0:
+                _isPenetrate = true;
+                break;
+            case 1:
+                _range = 300;
+                break;
+            default:
+                break;
+            }
+        }
+        IsUpgrade[level] = true;
     }
 }
