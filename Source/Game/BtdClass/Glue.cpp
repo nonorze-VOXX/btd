163afc0abd4ee068d00a960a7e0de829d69beca0
huhuhu0420 <ghostcheng.00@gmail.com>
2023-05-05 00:09:51 +0800

    glue class

---
+++ Source/Game/BtdClass/Glue.cpp
@@ -0,0 +1,71 @@
+﻿#include "stdafx.h"
+#include "Glue.h"
+
+namespace Btd
+{
+    Glue::Glue()
+    {
+    }
+
+    void Glue::LoadBitmapByString(vector<string> filepaths, COLORREF color)
+    {
+        tower.LoadBitmapByString(filepaths, color);
+        throwable.LoadBitmapByString(filepaths, color);
+    }
+    void Glue::SetCenter(int x, int y)
+    {
+        tower.SetCenter(x, y);
+        throwable.SetCenter(x, y);
+    }
+
+    void Glue::SetIsMove(bool isMove)
+    {
+        tower.SetIsMove(isMove);
+    }
+
+    void Glue::SetActive(bool active)
+    {
+        tower.SetActive(active);
+        throwable.SetActive(active);
+    }
+
+    bool Glue::IsMovable()
+    {
+        return tower.IsMovable();
+    }
+
+    void Glue::ShowBitmap()
+    {
+        tower.ShowBitmap();
+        throwable.ShowBitmap();
+        if (tower.IsMovable())
+        {
+            tower.RangeCircle.ShowBitmap(static_cast<float>(_range) / 100.0);
+        }
+    }
+
+    bool Glue::IsCursorFocus()
+    {
+        return tower.IsCursorFocus();
+    }
+
+    void Glue::SetClicked(bool clicked)
+    {
+        tower.SetClicked(clicked);
+    }
+
+    void Glue::SetDamageType(DamageType::DamageType damageType)
+    {
+        throwable.SetDamageType(damageType);
+    }
+
+    void Glue::DetectHitBalloon()
+    {
+        throwable.DetectHitBalloon();
+    }
+
+    void Glue::UpdateCantHitBloons()
+    {
+        throwable.UpdateCantHitBloons();
+    }
+}
---
+++ Source/Game/BtdClass/Glue.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "IceGas.h"
+#include "Tower.h"
+
+namespace Btd
+{
+    class Glue
+    {
+    public:
+        Glue();
+        void LoadBitmapByString(vector<string> filepaths, COLORREF color);
+        void SetCenter(int x, int y);
+        void SetIsMove(bool isMove);
+        void SetActive(bool active);
+        bool IsMovable();
+        void ShowBitmap();
+        bool IsCursorFocus();
+        void SetClicked(bool clicked);
+        void SetDamageType(DamageType::DamageType damageType);
+        void DetectHitBalloon();
+        void UpdateCantHitBloons();
+        
+        Tower tower;
+        IceGas throwable;
+    private:
+        int _range;
+    };
+}
