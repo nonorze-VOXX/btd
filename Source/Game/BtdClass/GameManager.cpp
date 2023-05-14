2abb7cfd8821145d4f85d7d7ca04beecbd23b3ac
nonorze-VOXX <appieosugood@gmail.com>
2023-05-14 03:01:11 -0700

    pop sound play, need cut for better experence

--- Source/Game/BtdClass/GameManager.cpp
+++ Source/Game/BtdClass/GameManager.cpp
@@ -3,6 +3,7 @@
 
 #include "BloonFactory.h"
 #include "TowerFactory.h"
+#include "../../Library/audio.h"
 
 
 namespace Btd
@@ -30,6 +31,21 @@ namespace Btd
 
     void GameManager::OnInit()
     {
+        vector<string> soundName ;
+        soundName.push_back("background.mp3");
+        soundName.push_back("pop.mp3");
+        soundName.push_back("lead.mp3");
+        game_framework::CAudio *audio = game_framework::CAudio::Instance();
+        for (int i =0 ;i< (int)soundName.size();i++)
+        {
+            // TODO FIXME not sure if it memery leak
+            string path = "Resources/sound/";
+            string local = path+soundName[i];
+            char* target = new char[local.length()+1];
+            strcpy(target,local.c_str());
+            audio->Load(i,target);
+        }
+        // audio->Play((int)BtdSound::BACKGROUND,true);
         GameFlow = Prepare;
         startButton.LoadBitmapByString({"resources/start_button.bmp"});
         startButton.SetTopLeft(742, 620);
