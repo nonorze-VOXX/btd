fee2d86bb4e74a2e1fff7eed41a2eda767c5a1b8
huhuhu0420 <ghostcheng.00@gmail.com>
2023-06-11 18:33:09 +0800

    bomb faster

--- Source/Game/BtdClass/Cannon.cpp
+++ Source/Game/BtdClass/Cannon.cpp
@@ -18,7 +18,7 @@ namespace Btd
     {
         auto bomb = make_shared<Bomb>(Bomb());
         bomb->LoadBitmapByString(ThrowablePath, RGB(255, 255, 255));
-        bomb->SetSpeed(5);
+        bomb->SetSpeed(8);
         throwablePool.push(bomb);
     }
 
