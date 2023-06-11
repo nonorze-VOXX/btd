c2b8f4a0c3cb39d1feb38a3c858611a456a0f73d
huhuhu0420 <ghostcheng.00@gmail.com>
2023-06-11 19:06:23 +0800

    clean .h file

--- Source/Game/BtdClass/Cannon.cpp
+++ Source/Game/BtdClass/Cannon.cpp
@@ -5,6 +5,17 @@
 
 namespace Btd
 {
+    Cannon::Cannon()
+    {
+        _range = 200;
+        shootDeltaTime = 6;
+        ThrowablePath = {"resources/towers/bomb/bomb.bmp", "resources/towers/bomb/bomb_1.bmp",
+            "resources/towers/bomb/bomb_2.bmp", "resources/towers/bomb/bomb_3.bmp", "resources/towers/bomb/bomb_4.bmp"};
+        UpgradePrice[0] = 380;
+        UpgradePrice[1] = 180;
+        SetDamageType(DamageType::Boom);
+    }
+
     void Cannon::Shoot(Vector2 target)
     {
         Tower::Shoot(target);
--- Source/Game/BtdClass/Super.h
+++ Source/Game/BtdClass/Super.h
@@ -7,20 +7,7 @@ namespace Btd
     class Super:public Tower
     {
     public:
-        Super()
-        {
-            _range = 100;
-            shootDeltaTime = 0.3f;
-            ThrowablePath = {
-            "resources/towers/monkey/dart_1.bmp", "resources/towers/monkey/dart_2.bmp",
-            "resources/towers/monkey/dart_3.bmp", "resources/towers/monkey/dart_4.bmp",
-            "resources/towers/monkey/dart_5.bmp", "resources/towers/monkey/dart_6.bmp",
-            "resources/towers/monkey/dart_7.bmp", "resources/towers/monkey/dart_8.bmp"
-            };
-            UpgradePrice[0] = 320;
-            UpgradePrice[1] = 200;
-            SetDamageType(DamageType::Normal);
-        }
+        Super();
         void Shoot(Vector2 target) override;
         void PushThrowablePool() override;
 		void Upgrade(int level) override;
